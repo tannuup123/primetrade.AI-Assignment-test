7334cd58ba9310970ea1863a43f8faa38bdd753b
Tanmay Upadhyay <tanmayup123@gmail.com>
2026-01-11 16:54:25 +0530

    Use env based API URL for Vercel

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,9 @@
-// api.js
+//api.js
 
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
